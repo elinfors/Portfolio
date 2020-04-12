1a10e921b1ba3553d85c42935fd978b97559f5f2
elinfors <elinfors@kth.se>
2020-04-12 14:33:15 +0200

    Updates

---
+++ precache-manifest.75556ccecb503c3754ffcb1d15c57f68.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e680a654387c5bf8dcb35387665e76",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e55f7222bc390a16554d",
+    "url": "/portfolio/static/css/main.165e2948.chunk.css"
+  },
+  {
+    "revision": "649314285f84f377b93f",
+    "url": "/portfolio/static/js/2.7407669c.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/portfolio/static/js/2.7407669c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55f7222bc390a16554d",
+    "url": "/portfolio/static/js/main.fabfe3d1.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "90d94db630eec05525ca58b400e47564",
+    "url": "/portfolio/static/media/CV-FOTO.90d94db6.png"
+  },
+  {
+    "revision": "11eda4a3fec6378bae8649bd87c642b0",
+    "url": "/portfolio/static/media/Elin Forsberg.11eda4a3.pdf"
+  },
+  {
+    "revision": "07fc946e86eef83beebb56b8d3907184",
+    "url": "/portfolio/static/media/carbon.07fc946e.PNG"
+  },
+  {
+    "revision": "695ee4f8049d9054185cbd17113dc826",
+    "url": "/portfolio/static/media/chat.695ee4f8.PNG"
+  },
+  {
+    "revision": "4d7e80980e9b33dc2a702893704090f9",
+    "url": "/portfolio/static/media/flight1.4d7e8098.PNG"
+  },
+  {
+    "revision": "9df15e03ae285f084baaa6998f2665df",
+    "url": "/portfolio/static/media/ikea.9df15e03.png"
+  }
+]);
