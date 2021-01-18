dc7a44d2f1c7c5d5ddc433a8373920ea4276934f
Elin <elinfors@kth.se>
2021-01-18 12:24:59 +0100

    fix

--- src/pages/Flight/flight.js
+++ src/pages/Flight/flight.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import "./flight.css"
+import '../projectStyle.css'
 import history from '../../history'
 import { useHistory } from "react-router-dom";
 
 import Footer from "../Footer/footer"
+var flight = require('./FlightBanner1.png')
 
 const Flight = () =>{
     let history = useHistory();
@@ -26,75 +28,99 @@ const Flight = () =>{
 
       
 
-    return(
+      return(
         <React.Fragment>
             <div className="menu sticky back">
                 {/*<img onClick={() => RedirectBack()} className="back_arrow"src={arrow}></img>*/}
                 <button onClick={() => RedirectBack()}  className="back_button">BACK</button>
             </div> 
-            <div className="coursePage">
+            <div className="page">
 
                 <div className="box6">
-                <div className="headlineBox">
-                        <i class="fas fa-chevron-left" onClick={()=>{RedirectKollin()}} ></i>
+                    <div className="headlineBox">
+                        <i class="fas fa-chevron-left" onClick={()=>{RedirectKollin()}}></i>
                         <div className="headline">
-                        Flight Visualization
+                        Flight
                         </div>
-                        <i className="fas fa-chevron-right" onClick={()=>{RedirectTripdoodler()}}></i>
+                        <i class="fas fa-chevron-right" onClick={()=>{RedirectTripdoodler()}}></i>
                     </div>
-                        <div className="words6">
-                    <div>
-                        Data Processing
-                    </div>
-                    <div>
-                       |
-                    </div>
-                    <div>
-                        Visualization
-                    </div>
-                    <div>
-                       |
-                    </div>
-                    <div>
-                        React.js
-                    </div>
-                    <div>
-                       |
-                    </div>
-                    <div>
-                        D3.js
-                    </div>
+                        <div className="words">
+                            <div>
+                                Web Development
+                            </div>
+                            <div>
+                            |
+                            </div>
+                            <div>
+                                Visualization
+                            </div>
+                            <div>
+                            |
+                            </div>
+                            <div>
+                                Data Processing
+                            </div>
+
+                        </div>
+                       
                 </div>
-                </div>
+                
                 <div className="projectContent">
-                <div className="projectDesc">
-                    Project Description
-                </div>
-                <div className="projectDesc">
-                    Problem
-                </div>
-                <div className="problem">
-                    <ul>
-               <li> Students found it difficult to find information about the course offerings at KTH.</li>
-                <li>Once found, the course information was solely presented through text and a full understanding of course content and structure was restricted.
-               </li>
-               <li> Students found it difficult to get an overview of their education. 
-               </li>
-                </ul>
-                </div>
-                 <div className="projectDesc">
-                    Solution
-                </div>
-                <div className="solution">
 
-                The design and development of an interactive visualization tool for customization and personalization of KTH students’ educational paths
-                </div>
-                <div className="projectDesc">
-                    Design Process
-                </div>
-                <div className="design process">
+                    <div className="mediaDiv">
+                        <img className="solutionImg"src={flight}></img>
 
-                The design and development of an interactive visualization tool for customization and personalization of KTH students’ educational paths
+
+                    </div>
+                    <div className="descriptionDiv">
+                        <div className="infoIcons">
+                                <div className="infoDivHead">
+                                        <p style={{fontSize:'15px'}}>DATA VISUALIZATION</p>
+                                        <p className="projectName">Flight</p>
+                                    </div>
+                                    <div className="infoDiv">
+                                        <i class="fas fa-user-tag"></i>
+                                        <p>Designer</p>
+                                    </div>
+                                    <div className="infoDiv">
+                                        <i class="fas fa-users"></i>
+                                        <p>3</p>
+                                    </div>
+                                    <div className="infoDiv">
+                                        <i class="fas fa-wrench"></i>
+                                        <p></p>
+                                    </div>
+                        </div>
+
+                        <div className="projectDesc">
+                            Project Description
+                        </div>
+                        <div className="projectDesc">
+                            Problem
+                        </div>
+                        <div className="projectText">
+                            <ul>
+                    <li> Students found it difficult to find information about the course offerings at KTH.</li>
+                        <li>Once found, the course information was solely presented through text and a full understanding of course content and structure was restricted.
+                    </li>
+                    <li> Students found it difficult to get an overview of their education. 
+                    </li>
+                        </ul>
+                        </div>
+                        <div className="projectDesc">
+                            Solution
+                        </div>
+                        <div className="projectText">
+
+                        The design and development of an interactive visualization tool for customization and personalization of KTH students’ educational paths
+                        </div>
+                        <div className="projectDesc">
+                            Design Process
+                        </div>
+                        <div className="projectText">
+
+                        The design and development of an interactive visualization tool for customization and personalization of KTH students’ educational paths
+                        </div>
                 </div>
                 </div>
 
