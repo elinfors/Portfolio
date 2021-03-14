4b54eea995b10a2b295872ab799555f9c5c5eab5
Elin <elinfors@kth.se>
2021-03-14 17:19:03 +0100

    fix

---
+++ build/precache-manifest.59667961d7b0be0b186db3e2baf193d1.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748c0519804f5f089fd3759a5e05eea8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b5987dfc662e7aeed5a",
+    "url": "/portfolio/static/css/main.dae50920.chunk.css"
+  },
+  {
+    "revision": "a441b448130887891337",
+    "url": "/portfolio/static/js/2.bdb0a406.chunk.js"
+  },
+  {
+    "revision": "728a5ed59fc0638b030a3df30164ca48",
+    "url": "/portfolio/static/js/2.bdb0a406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5987dfc662e7aeed5a",
+    "url": "/portfolio/static/js/main.c5995fd1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "58a55123b51b5f88477c66a584acfe55",
+    "url": "/portfolio/static/media/CourseBanner1.58a55123.png"
+  },
+  {
+    "revision": "58c9520a218ab8dc655cbb8af543e665",
+    "url": "/portfolio/static/media/CourseProcess.58c9520a.png"
+  },
+  {
+    "revision": "680277dfd894f2e012f907ef1a3a6abc",
+    "url": "/portfolio/static/media/CourseSolution.680277df.png"
+  },
+  {
+    "revision": "f4896afa65b816912852a090427509c8",
+    "url": "/portfolio/static/media/Coursearch.f4896afa.png"
+  },
+  {
+    "revision": "8b5dbe5bdf9ca91b624399763835f5f6",
+    "url": "/portfolio/static/media/Elin Forsberg CV.8b5dbe5b.pdf"
+  },
+  {
+    "revision": "11eda4a3fec6378bae8649bd87c642b0",
+    "url": "/portfolio/static/media/Elin Forsberg.11eda4a3.pdf"
+  },
+  {
+    "revision": "68eb9f1606f8ed3257b44a04cfa45ec9",
+    "url": "/portfolio/static/media/Figma.68eb9f16.PNG"
+  },
+  {
+    "revision": "a9268f6514e1bda4e1741adda366824b",
+    "url": "/portfolio/static/media/Flight.a9268f65.png"
+  },
+  {
+    "revision": "579826e6610d0cb1a2a994d81244272a",
+    "url": "/portfolio/static/media/FlightBanner1.579826e6.png"
+  },
+  {
+    "revision": "8022474d5b6a6ae3db0f170e93187b4f",
+    "url": "/portfolio/static/media/FlightSolution2.8022474d.png"
+  },
+  {
+    "revision": "12ce18dad41737a4f649c81f386f9452",
+    "url": "/portfolio/static/media/FrontBanner3.12ce18da.png"
+  },
+  {
+    "revision": "9381b1d38376a6ed4866f10ab0e32502",
+    "url": "/portfolio/static/media/Frontread Project - Report.9381b1d3.pdf"
+  },
+  {
+    "revision": "5db4ef31ff72c54c4f4a551f37c7b784",
+    "url": "/portfolio/static/media/Frontread2.5db4ef31.png"
+  },
+  {
+    "revision": "7da260bd9e7ce7ce7f9cb9e05a72effc",
+    "url": "/portfolio/static/media/HomeBanner2.7da260bd.jpg"
+  },
+  {
+    "revision": "eef2eb832a67b72a31624b75666c51bd",
+    "url": "/portfolio/static/media/HomeBannerSmall.eef2eb83.png"
+  },
+  {
+    "revision": "1361012187417d9ccfc9a6684b81525b",
+    "url": "/portfolio/static/media/IKEAvideo.13610121.mp4"
+  },
+  {
+    "revision": "f8bddd8e868cf9a81dc5581e2295dd95",
+    "url": "/portfolio/static/media/IkeaSolution.f8bddd8e.png"
+  },
+  {
+    "revision": "50d676545deeed5b2fee327d651aded9",
+    "url": "/portfolio/static/media/KTH Coursearch Project Report .50d67654.pdf"
+  },
+  {
+    "revision": "170b1dfed3b30d6e44b21c4e6bbaa046",
+    "url": "/portfolio/static/media/Kollin3.170b1dfe.png"
+  },
+  {
+    "revision": "a704da97c3c23f499cda60a0635f262e",
+    "url": "/portfolio/static/media/KollinBanner.a704da97.png"
+  },
+  {
+    "revision": "a7b9eedc2c7bf19c32d0d3bf590040a6",
+    "url": "/portfolio/static/media/KollinSolution.a7b9eedc.png"
+  },
+  {
+    "revision": "6ba3029c12a417fe2ec509a7c697b4fe",
+    "url": "/portfolio/static/media/Nearby2.6ba3029c.png"
+  },
+  {
+    "revision": "dc4f5b6f5e9f390ac3a3612f95c5b6b7",
+    "url": "/portfolio/static/media/NearbyBanner.dc4f5b6f.png"
+  },
+  {
+    "revision": "d3b32e5e9c23842f3e94b3910d387fae",
+    "url": "/portfolio/static/media/NearbySolution.d3b32e5e.png"
+  },
+  {
+    "revision": "f8f7a914d8cb6319feeda540413c192d",
+    "url": "/portfolio/static/media/Project Report - NearBy.f8f7a914.pdf"
+  },
+  {
+    "revision": "6eba66133d66df2fbe2203908b29cf86",
+    "url": "/portfolio/static/media/Requirements.6eba6613.png"
+  },
+  {
+    "revision": "c97e6f8d5eb7f3c0c9a37a62ea4596f2",
+    "url": "/portfolio/static/media/Sketching.c97e6f8d.png"
+  },
+  {
+    "revision": "eec444f531902a493930b4db05cd108d",
+    "url": "/portfolio/static/media/Survey.eec444f5.png"
+  },
+  {
+    "revision": "7a2099f1fd26f5d412b26349ec395b7f",
+    "url": "/portfolio/static/media/TripBanner2.7a2099f1.png"
+  },
+  {
+    "revision": "2e87b7f83030bc6ad80f6f9e92b25da5",
+    "url": "/portfolio/static/media/TripFlow.2e87b7f8.png"
+  },
+  {
+    "revision": "426f83d54bd63378d574b90dd564e985",
+    "url": "/portfolio/static/media/TripProcess.426f83d5.png"
+  },
+  {
+    "revision": "209b1667aa4934269444d6b525d8915b",
+    "url": "/portfolio/static/media/TripSketch.209b1667.png"
+  },
+  {
+    "revision": "5a5cd8ca45789976a8309b9df4c9bef2",
+    "url": "/portfolio/static/media/TripSolution.5a5cd8ca.png"
+  },
+  {
+    "revision": "cb15b49bc2e59d54acceaeaa3fd7d50e",
+    "url": "/portfolio/static/media/TripSolution2.cb15b49b.png"
+  },
+  {
+    "revision": "b669bbdcb29ad7b18538c1cf4bcaaf05",
+    "url": "/portfolio/static/media/TripUSM.b669bbdc.png"
+  },
+  {
+    "revision": "c0d7d6452458cb38f108cc184e0e1898",
+    "url": "/portfolio/static/media/TripVid.c0d7d645.mp4"
+  },
+  {
+    "revision": "9c9dd1931485331c8bd00c0535b0e83b",
+    "url": "/portfolio/static/media/Tripdoodler2.9c9dd193.png"
+  },
+  {
+    "revision": "d40b25988c8626e190b6b3c642f2c965",
+    "url": "/portfolio/static/media/UserFlow.d40b2598.png"
+  },
+  {
+    "revision": "872f406202b3d4ee87087099c7e770ad",
+    "url": "/portfolio/static/media/carbon2.872f4062.png"
+  },
+  {
+    "revision": "695ee4f8049d9054185cbd17113dc826",
+    "url": "/portfolio/static/media/chat.695ee4f8.PNG"
+  },
+  {
+    "revision": "c808e4ea8d6f17e50b598db33c02d180",
+    "url": "/portfolio/static/media/coursearch1.c808e4ea.png"
+  },
+  {
+    "revision": "0869ba34c49045f6e28c3f9a8bf00b62",
+    "url": "/portfolio/static/media/cvfoto.0869ba34.jpg"
+  },
+  {
+    "revision": "555f49b9175ccf49c5d91ddb658fca01",
+    "url": "/portfolio/static/media/feedback.555f49b9.png"
+  },
+  {
+    "revision": "69b91ba3e1dd189c5f4675bc47069072",
+    "url": "/portfolio/static/media/flight2.69b91ba3.png"
+  },
+  {
+    "revision": "b8bf177fdb4718ff40bc0704cc063a6b",
+    "url": "/portfolio/static/media/goal.b8bf177f.png"
+  },
+  {
+    "revision": "5e32e2e21d5b92c9d927155a90422b40",
+    "url": "/portfolio/static/media/ikea.5e32e2e2.png"
+  },
+  {
+    "revision": "934523a18cd193e518eb8f1c483c38d2",
+    "url": "/portfolio/static/media/ikeaHeader.934523a1.png"
+  },
+  {
+    "revision": "d1199d8e5de1f7b1a4e194edafd36e6e",
+    "url": "/portfolio/static/media/ikeaWorkbook.d1199d8e.pdf"
+  },
+  {
+    "revision": "4c78fe8e4a8c86e0fe5f8b4a77104963",
+    "url": "/portfolio/static/media/nearbyMovie.4c78fe8e.mp4"
+  },
+  {
+    "revision": "f6e12c671a0f1cb6aa224d6d62f77eab",
+    "url": "/portfolio/static/media/overview.f6e12c67.png"
+  },
+  {
+    "revision": "6dc8ca56d61b52a1d0aa8a7fcc0f5cff",
+    "url": "/portfolio/static/media/progress.6dc8ca56.png"
+  },
+  {
+    "revision": "cb3c2545fa44d10cbbbead46f91090f2",
+    "url": "/portfolio/static/media/theme1.cb3c2545.png"
+  },
+  {
+    "revision": "18d6421a4c5023333d8b5dc945439580",
+    "url": "/portfolio/static/media/theme2.18d6421a.png"
+  }
+]);
