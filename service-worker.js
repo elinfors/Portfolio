1b94cc4ddc0e8cb7fc7329eb9766ff8e6828b83d
Elin <elinfors@kth.se>
2021-03-15 20:35:36 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a54895b2e0faed320837457603f6b6e9.js"
+  "/portfolio/precache-manifest.6859c015a81f22bb113f7497810d56d9.js"
 );
 
 self.addEventListener('message', (event) => {
